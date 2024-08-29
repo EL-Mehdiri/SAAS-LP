665fd0de64382f98c74964f334094453f1e8e963
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-08-29 18:41:34 +0100

    "Refactor globals.css, simplify page.tsx, and add Header component"

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,18 @@
-export const Navbar = () => {
-  return null;
+import Image from "next/image";
+export const Header = () => {
+  return (
+    <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+      <div className="inline-flex gap-1 items-center">
+        <p>Get started for free</p>
+        <div className="h-4 w-4 inline-flex justify-center items-center">
+          <Image
+            width={4}
+            height={4}
+            src={"../assets/arrow-right.svg"}
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
