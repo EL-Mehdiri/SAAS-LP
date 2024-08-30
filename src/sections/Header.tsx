32ae503eb1315e57cbf9f521c525fcece0da62bb
Mehdi Ridmani <117091960+EL-Mehdiri@users.noreply.github.com>
2024-08-30 15:14:49 +0100

    "Added Hero component, updated Header component, and modified globals.css and tailwind.config.ts files"

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,7 +5,7 @@ import MenuIcon from "@/assets/menu.svg";
 import Image from "next/image";
 export const Header = () => {
   return (
-    <header className="sticky top-0 ">
+    <header className="sticky top-0 z-20  backdrop-blur-sm">
       <div className="flex justify-center gap-3 items-center py-3  bg-black text-white text-sm">
         <p className="text-white/60 hidden md:block ">
           Stremline your workflow and boost your productivity
@@ -15,7 +15,7 @@ export const Header = () => {
           <ArrowRight className="h-4 w-4 inline-flex justify-center items-center" />
         </div>
       </div>
-      <div className="py-5">
+      <div className="py-5 ">
         <div className="container">
           <div className="flex items-center justify-between ">
             <Image src={Logo} alt="logo" height={40} width={40} />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,15 @@ const config: Config = {
       },
       container: {
         center: true,
+        padding: {
+          DEFAULT: "20px",
+          lg: "80px",
+        },
+      },
+      screens: {
+        sm: "375px",
+        md: "768px",
+        lg: "1200px",
       },
     },
   },
